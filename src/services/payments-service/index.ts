00dd54edf98ce22653e40ba6231cfe80cf06da85
guilhiz <guilhermevilelasobral@gmail.com>
2023-04-15 14:07:40 -0300

    fix: returned when ticketId is not associated with user

--- src/services/payments-service/index.ts
+++ src/services/payments-service/index.ts
@@ -1,17 +1,18 @@
 import { Payment } from '@prisma/client';
 import paymentsRepository from '@/repositories/payments-repository';
-import enrollmentRepository from '@/repositories/enrollment-repository';
 import ticketsRepository from '@/repositories/tickets-repository';
 import { invalidDataError, notFoundError, unauthorizedError } from '@/errors';
 import { PaymentsProcessProps } from '@/protocols';
 
 async function getPayment(ticketId: number, userId: number) {
   if (!ticketId) throw invalidDataError(['Ticket ID must be provided']);
-  const enrollment = await enrollmentRepository.findWithAddressByUserId(userId);
+  const ticket = await ticketsRepository.findTicketByTicketId(ticketId);
+
+  if (!ticket) throw notFoundError();
+
+  if (ticket.Enrollment.userId !== userId) throw unauthorizedError();
 
   const payment = await paymentsRepository.findPaymentByTicketId(ticketId);
-
-  if (!payment) throw notFoundError();
 
   return { payment };
 }
